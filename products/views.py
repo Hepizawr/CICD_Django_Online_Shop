e515cf8fa7e055c11b3f07cb329208421601a179
Max Boichuk <boychuk.mk@gmail.com>
2023-05-02 14:26:09 +0300

    static files added + main_page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+import random
+
+
+class Product:
+
+    def __init__(self):
+        self.title = 'T-shirt'
+        self.category = random.choice(['Male', 'Female', 'Girl', 'Boy'])
+        self.price = random.randint(10, 1000)
+
+def main_page(request):
+
+    products = [Product() for _ in range(20)]
+    #products = list(range(20))
+    return render(request, 'products_list.html', context={"products":products})
