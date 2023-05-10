838b77aaafb375a3ac7958f6339feb2ba56d58f7
Hepizawr <Denis.diachenko.2000@gmail.com>
2023-05-10 19:59:16 +0300

    Add category panel at products page

--- products/views.py
+++ products/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Discount
 
 
 # Create your views here.
 def index(request):
     context = {
         'title': "Famms",
-        'products': Product.objects.all()
+        'products': Product.objects.all()[:8],
+        'discounts': Discount.objects.all()[:2]
     }
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
+    if category_id:
+        products = Product.objects.filter(category=category_id)
+    else:
+        products = Product.objects.all()
     context = {
         'title': "Famms - products",
-        'products': Product.objects.all(),
+        'products': products,
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
