de75323f30e83f13c4acbd4cb6e5bd7e534716b9
Max Boichuk <boychuk.mk@gmail.com>
2023-05-15 23:31:17 +0300

    additional settings

--- CICD_Django_Online_Shop/settings.py
+++ CICD_Django_Online_Shop/settings.py
@@ -128,6 +128,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # User
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
